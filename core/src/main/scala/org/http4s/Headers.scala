526d5ec190febf4e811104c4899a4b8934baf3a5
Ross A. Baker <baker@alumni.indiana.edu>
2013-01-25 22:57:57 -0500

    Headers are a Seq.

--- core/src/main/scala/org/http4s/Headers.scala
+++ core/src/main/scala/org/http4s/Headers.scala
@@ -1,11 +1,35 @@
 package org.http4s
 
-trait Headers extends Seq[Header] {
-  def contentType: Option[ContentType]
-  def contentLength: Option[Long]
+import collection.{mutable, immutable}
+import scala.collection.generic.CanBuildFrom
+
+class Headers private(headers: Seq[Header])
+  extends immutable.Seq[Header]
+  with collection.SeqLike[Header, Headers]
+{
+  override protected[this] def newBuilder: mutable.Builder[Header, Headers] = Headers.newBuilder
+
+  def length: Int = headers.length
+
+  def apply(idx: Int): Header = headers(idx)
+
+  def iterator: Iterator[Header] = headers.iterator
+
+  lazy val contentLength: Option[Long] = None
+  lazy val contentType: Option[ContentType] = None
 }
 
-trait Header {
-  def name: String
-  def value: String
+object Headers {
+  def apply(headers: Header*): Headers = new Headers(headers)
+
+  implicit def canBuildFrom: CanBuildFrom[Headers, Header, Headers] =
+    new CanBuildFrom[Headers, Header, Headers] {
+      def apply(from: Headers): mutable.Builder[Header, Headers] = newBuilder
+      def apply(): mutable.Builder[Header, Headers] = newBuilder
+    }
+
+  private def newBuilder: mutable.Builder[Header, Headers] =
+    new mutable.ListBuffer[Header] mapResult(xs => new Headers(xs))
 }
+
+case class Header(name: String, value: String)
